8fa864484f03ea3f327aab4c55c19055662dad44
Наталья <nata.salimowa2015@yandex.ru>
2021-06-18 01:24:51 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-import {createAdvertisement} from './data.js';
 import './change-state.js';
 import './form/form.js';
 import './map.js';
-
-const SIMILAR_ADVERTISEMENT_COUNT = 10;
-
-new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(() => createAdvertisement());
---
+++ js/map.js
@@ -0,0 +1,1 @@
+import './create-similar-element.js';
